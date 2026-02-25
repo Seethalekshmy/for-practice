1dc23385b3cdbc265d03cc4437dc537b7f7606f6
Seetha Lekshmi <seethalekshmi2005@gmail.com>
2026-02-25 20:16:53 +0530

    deleted

--- char_frequency.py
+++
@@ -1,9 +0,0 @@
-#to find frequency of characterin a string
-string = input("enter the string:")
-char = input ("enter the character to find frequency of: ")
-freq_char = 0
-for i in string:
-    if i == char:
-        freq_char += 1
-    
-print ("frequency of character",char,"string",string,"is",freq_char)
