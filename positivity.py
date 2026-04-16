5b9684d1b7d19d20ca96697c468756fb419d2389
Seetha Lekshmi <seethalekshmi2005@gmail.com>
2026-04-16 08:50:36 +0530

    mood classifier

---
+++ positivity.py
@@ -0,0 +1,6 @@
+#from the given moods print the positive moods
+mood = ["sad","calm","happy","angry"]
+positive_moods = ["happy","calm"]
+for moods in mood:
+    if moods in positive_moods:
+        print(moods)
